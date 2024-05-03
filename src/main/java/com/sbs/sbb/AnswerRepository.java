3240693d95ee981c9a4717a473bd311ddec0deb9
Junsu-Ahn <ajunsu5@gmail.com>
2024-05-03 16:43:26 +0900

    repository

---
+++ src/main/java/com/sbs/sbb/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.sbs.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
---
+++ src/main/java/com/sbs/sbb/QuestionRepository.java
@@ -0,0 +1,6 @@
+package com.sbs.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class QuestionRepository extends JpaRepository<Question, Integer> {
+}
